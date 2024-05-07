129357ce9361a872fef4d86ce8df183d336f4818
BinTianqi <bintianqi512@gmail.com>
2024-05-07 08:53:19 +0800

    remove unused resources

--- app/src/main/java/com/bintianqi/nip/ui/activity/base/BaseActivity.kt
+++ app/src/main/java/com/bintianqi/nip/ui/activity/base/BaseActivity.kt
@@ -2,14 +2,9 @@
 
 package com.bintianqi.nip.ui.activity.base
 
-import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.res.ResourcesCompat
-import androidx.core.view.WindowCompat
 import androidx.viewbinding.ViewBinding
-import com.bintianqi.nip.R
-import com.bintianqi.nip.utils.factory.isNotSystemInDarkMode
 import com.highcapable.yukihookapi.hook.factory.current
 import com.highcapable.yukihookapi.hook.factory.method
 import com.highcapable.yukihookapi.hook.type.android.LayoutInflaterClass
@@ -24,15 +19,8 @@ abstract class BaseActivity<VB : ViewBinding> : AppCompatActivity() {
         }?.get()?.invoke<VB>(layoutInflater) ?: error("binding failed")
         setContentView(binding.root)
         supportActionBar?.hide()
-        WindowCompat.getInsetsController(window, window.decorView).apply {
-            isAppearanceLightStatusBars = isNotSystemInDarkMode
-            isAppearanceLightNavigationBars = isNotSystemInDarkMode
-        }
-        ResourcesCompat.getColor(resources, R.color.colorThemeBackground, null).also {
-            window?.statusBarColor = it
-            window?.navigationBarColor = it
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) window?.navigationBarDividerColor = it
-        }
+        window?.statusBarColor = 0
+        window?.navigationBarColor = 0
         onCreate()
     }
     abstract fun onCreate()
